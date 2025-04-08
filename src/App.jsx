2747a2318fccb3a9aeab868577d4b1d62d574af5
A <surya.b.a@capgemini.com>
2025-04-08 07:03:14 +0530

    Added H1

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h1>OK Start</h1>
       </header>
       <MainContent />
     </>
